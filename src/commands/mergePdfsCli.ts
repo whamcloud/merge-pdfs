d4eedf1f1c5573a8f2555d584329c67fe59cc9aa
Jonathan Parris <jparris@ddn.com>
2024-07-15 16:31:38 -0600

    Initial URL Rewrite

--- src/commands/mergePdfsCli.ts
+++ src/commands/mergePdfsCli.ts
@@ -10,17 +10,34 @@ import { name, version } from "../../package.json";
 export async function mergePdfsCli() {
 	const program = yargs(hideBin(process.argv)).scriptName(name)
 		.usage("$0 <entry...>")
-		.option("o", {
+		.options({
+			'o': {
 			alias: "output",
 			default: "merged-pdf.pdf",
 			describe: "Output file",
 			type: "string",
 			demandOption: false,
-		})
+		},
+		'u': {
+			alias: "urls",
+			default: [],
+			describe: "The source URLs for each PDF. Used to rewrite internal PDF links",
+			type: "array",
+			demandOption: false,
+		},
+		'b': {
+			alias: "base_url",
+			default: "",
+			describe: "Only PDF links that begin with this base_url will be rewritten",
+			type: "string",
+			demandOption: false,
+		}
+	})
 		.example([
 			["$0 1.pdf 2.pdf", "Merge two PDFs files"],
 			["$0 1.pdf 2.pdf -o merged-pdf.pdf", "Merge two PDFs files into merged-pdf.pdf file"],
 			["$0 pdfs/*.pdf -o merged-pdf.pdf", "Merge some PDFs files into merged-pdf.pdf file"],
+			["$0 1.pdf 2.pdf -u http://localhost:16762/2.html http://localhost:16762/2.html -b http://localhost:16762 ", "Merge files rewritting internal links"],
 		])
 		.showHelpOnFail(false)
 		.alias("h", "help")
@@ -30,8 +47,10 @@ export async function mergePdfsCli() {
 	const argv = await program.argv;
 	const entry = argv._;
 	let output = argv.o;
+	let base_url = argv.b;
+	let urls = argv.u;
 
-	const outBuffer = await mergePDFs(entry as string[]);
+	const outBuffer = await mergePDFs(entry as string[], urls as string[], base_url);
 	if (!output.endsWith(".pdf"))
 		output += ".pdf";
 
--- src/mergePDFs.ts
+++ src/mergePDFs.ts
@@ -18,7 +18,7 @@ export const MOUNT_DIR = "/" + "991e729a-8f2e-472a-8402-c26bb03b5ea3";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-export async function mergePDFs(entry: string[]) {
+export async function mergePDFs(entry: string[], urls: string[], base_url: String) {
 	const dir = process.cwd();
 	const absolutePathArr = fg.sync(entry, {
 		ignore: ["node_modules"],
@@ -32,6 +32,11 @@ export async function mergePDFs(entry: string[]) {
 		process.exit(1);
 	}
 
+	if (urls.length > 0 && absolutePathArr.length != urls.length){
+		process.stdout.write(yellowBright("Each PDF must have a URL.\n"));
+		process.exit(1);
+	}
+
 	const pyodide = await loadPyodide({ indexURL: resolve(__dirname, "pyodide") });
 	await pyodide.loadPackage("micropip");
 	const micropip = pyodide.pyimport("micropip");
@@ -42,7 +47,7 @@ export async function mergePDFs(entry: string[]) {
 	 * @see https://github.com/pyodide/pyodide/issues/3246#issuecomment-1312210155
 	 * You need to prefix the path with emfs: or it will be treated as a url
 	*/
-	await micropip.install(`emfs:${MOUNT_DIR}/pyodide/pypdf-3.8.1-py3-none-any.whl`);
+	await micropip.install(`emfs:${MOUNT_DIR}/pyodide/pypdf-4.3.0-py3-none-any.whl`);
 
 	const tempFileNameArr: string[] = [];
 	absolutePathArr.forEach((filePath) => {
@@ -54,7 +59,9 @@ export async function mergePDFs(entry: string[]) {
 	const curDate = formatDate(new Date());
 
 	await pyodide.runPythonAsync(`
-		from pypdf import PdfWriter
+		from pypdf import PdfWriter, PdfReader
+		from pypdf.annotations import Link
+		from pypdf.generic import Fit
 		from json import loads
 
 		writer = PdfWriter()
@@ -67,8 +74,98 @@ export async function mergePDFs(entry: string[]) {
 				}
 		)
 
-		for path in loads('${JSON.stringify(tempFileNameArr)}'):
-			writer.append(path)
+		pdfs = loads('${JSON.stringify(tempFileNameArr)}')
+		urls = loads('${JSON.stringify(urls)}')
+
+		if len(urls) == 0:
+			for path in pdfs:
+				writer.append(path)
+		else:
+			url_to_page = {}
+
+			# Merge the PDFs into a single document
+			for pdf, url in zip(pdfs, urls):
+					url_to_page[url] = writer.get_num_pages()
+					writer.append(pdf)
+
+			# Go through the Annotations
+			to_add = []
+			to_del = []
+			for page_no, page in enumerate(writer.pages):
+					if "/Annots" in page:
+							for annot_id in page["/Annots"]:
+									annot = annot_id.get_object()
+									if "/A" in annot:
+											if "/URI" in annot["/A"] and annot["/A"]["/URI"].startswith(
+													"${base_url}"
+											):
+													url = annot["/A"]["/URI"]
+													page = url.split("/")[-1]
+													# HTML anchor are complied to PDF named destinations
+													if "#" in page:
+															# PDF named desinations have a leading "/"
+															anchor = "/" + page.split("#")[-1]
+															if anchor in writer.named_destinations:
+																	named_dest = writer.named_destinations[anchor]
+																	dest_page = writer.get_page_number(
+																			named_dest["/Page"].get_object()
+																	)
+																	# Create a link based on the named destination
+																	to_add.append(
+																			(
+																					page_no,
+																					Link(
+																							border=annot["/Border"],
+																							rect=annot["/Rect"],
+																							target_page_index=dest_page,
+																							fit=Fit(
+																									fit_type=named_dest["/Type"],
+																									fit_args=(
+																											named_dest["/Left"],
+																											named_dest["/Top"],
+																											named_dest["/Zoom"],
+																									),
+																							),
+																					),
+																			)
+																	)
+																	to_del.append((page_no, annot_id))
+															else:
+																	print(
+																			"Problem anchor on page {}: {}".format(
+																					page_no, annot
+																			)
+																	)
+													else:
+															# For URLS that end in "/", a.k.a without a page, we set the page to index.html
+															if page == "":
+																	url += "index.html"
+
+															if url in url_to_page:
+																	# Convert link to to a URL
+																	to_add.append(
+																			(
+																					page_no,
+																					Link(
+																							border=annot["/Border"],
+																							rect=annot["/Rect"],
+																							target_page_index=url_to_page[url],
+																					),
+																			)
+																	)
+																	to_del.append((page_no, annot_id))
+															else:
+																	print(
+																			"Problem link on page {}: {}".format(page_no, annot)
+																	)
+
+			for page_num, annotation in to_del:
+					del writer.pages[page_num]["/Annots"][
+							writer.pages[page_num]["/Annots"].index(annotation.indirect_reference)
+					]
+
+			for page_num, annotation in to_add:
+					writer.add_annotation(page_number=page_num, annotation=annotation)
 
 		writer.write("/${MERGE_PDF_NAME}")
 		writer.close()
